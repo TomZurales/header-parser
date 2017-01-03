07fc285c13b72b2502fd79b86dcdd1c80f3edec7
TomZurales <tom@TomPC.Tom>
2017-01-03 11:42:59 -0600

    Made Heroku compatible and added landing page

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var express = require('express');
+var path = require('path')
 var app = express();
 
 var ip, language, software;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('port', (process.env.PORT || 5000));
+
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
 app.get('/', function(req, res){
    res.writeHead(200, {'Content-Type': 'application/json'});
    ip = req.headers['x-forwarded-for'];
@@ -10,4 +21,6 @@ app.get('/', function(req, res){
    res.end(JSON.stringify({'ip': ip, 'language': language, 'software': software}));
 });
 
-app.listen(8080);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
